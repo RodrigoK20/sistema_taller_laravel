f5576d2a5f97351e96866f22633d76b3ba733117
RodrigoK20 <rviscarra2297@gmail.com>
2021-04-25 23:32:06 -0600

    registro rapido cliente y vehiculo

--- resources/views/admin/cotizacion/create.blade.php
+++ resources/views/admin/cotizacion/create.blade.php
@@ -9,6 +9,10 @@
     </a>
 </li>
 
+<a class="nav-link" type="button" data-toggle="modal" data-target="#exampleModal-3">
+      <span class="btn btn-info ml-10">+ Registrar Vehículo</span>
+    </a>
+
 
 @endsection
 
@@ -16,11 +20,11 @@
 <div class="content-wrapper">
     <div class="page-header">
         <h3 class="page-title">
-            Venta
+            Cotización
         </h3>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{route('sales.index')}}">Listado Ventas</a></li>
+                <li class="breadcrumb-item"><a href="{{route('cotizacions.index')}}">Cotizaciones</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Registrar</li>
             </ol>
         </nav>
@@ -65,7 +69,7 @@
 
 
 
-
+<!-- MODAL CLIENTE -->
 <div class="modal fade" id="exampleModal-2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel-2"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -121,6 +125,98 @@
         </div>
     </div>
 </div>
+
+
+<!-- Modal Vehiculo -->
+
+<div class="modal fade" id="exampleModal-3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel-3"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel-2">Registro rápido de Vehiculo</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+
+            {!! Form::open(['route'=>'cars.store', 'method'=>'POST','files' => true]) !!}
+
+
+            <div class="modal-body">
+
+                <div class="form-group">
+                    <label for="name">Nombre</label>
+                    <input type="text" class="form-control" name="brand" id="" placeholder="Marca" required>
+                    @if ($errors->has('brand'))
+                    <small class="text-danger">{{ $errors->first('brand') }}</small>
+                 @endif
+                </div>
+                <div class="form-group">
+                    <label for="model">Modelo</label>
+                    <input type="text" class="form-control" name="model" id="model" placeholder="Modelo" required>
+                    @if ($errors->has('model'))
+                    <small class="text-danger">{{ $errors->first('model') }}</small>
+                    @endif
+                </div>
+
+                
+        <div class="form-group">
+            <label for="license_plate">Placa</label>
+            <input type="text" class="form-control" name="license_plate" id="" placeholder="Placa" max="8" required>
+            @if ($errors->has('license_plate'))
+                    <small class="text-danger">{{ $errors->first('license_plate') }}</small>
+             @endif
+        </div>
+
+                      
+        <div class="form-group">
+            <label for="year">Año</label>
+            <input type="text" class="form-control" name="year" id=""  placeholder="Año" maxlength="7" minlength="4"  required>
+            @if ($errors->has('year'))
+                    <small class="text-danger">{{ $errors->first('year') }}</small>
+             @endif
+        </div>
+
+        <div class="form-group">
+            <label for="year">Viscosidad</label>
+            <input type="text" class="form-control" name="viscosity" id="" placeholder="Viscosidad">
+            <small id="helpId" class="form-text text-muted">Este campo es opcional.</small>
+            @if ($errors->has('viscosity'))
+                    <small class="text-danger">{{ $errors->first('viscosity') }}</small>
+             @endif
+        </div>
+
+        
+                      
+        <div class="form-group">
+            <label for="year">Cliente</label>
+            <select class="form-control" name="client_id" id="client_id">
+                        @foreach ($clients as $client)
+                        <option value="{{$client->id}}">{{$client->name}}</option>
+                        @endforeach
+                      </select>
+            @if ($errors->has('year'))
+                    <small class="text-danger">{{ $errors->first('year') }}</small>
+             @endif
+        </div>
+
+    
+         <input type="hidden" name="sale" value="1">
+
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-success">Registrar</button>
+                <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
+            </div>
+
+        {!! Form::close() !!}
+
+        </div>
+    </div>
+</div>
+
 
 
 <!-- Modal Cambio !-->
